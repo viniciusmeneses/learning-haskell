5418064c0f73613dd2d03b6b40e9cc8b7b3f4b45
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-08-15 20:20:05 -0300

    feat: add exercises from class 7

--- exercises.hs
+++ exercises.hs
@@ -1,6 +1,3 @@
-import Language.Haskell.TH (safe)
--- Course: https://www.youtube.com/playlist?list=PLF1Z-APd9zK7usPMx3LGMZEHrECUGodd3
-
 -- Class 3
 -- double x = x + x
 quadruple y = double (double y)
@@ -73,3 +70,22 @@ _ || _ = True
 (&&) :: Bool -> Bool -> Bool
 -- x && y = if x then if y then True else False else False
 x && y = if x then y else False
+
+-- Class 7
+pyth :: Int -> Int -> Int -> Bool
+pyth x y z = x^2 + y^2 == z^2
+
+pyths :: Int -> [(Int, Int, Int)]
+pyths n = [(x, y, z) | x <- [1..n], y <- [1..n], z <- [1..n], pyth x y z]
+
+factors :: Int -> [Int]
+factors n = [x | x <- [1..n], n `mod` x == 0]
+
+perfect :: Int -> Bool
+perfect n = sum (init (factors n)) == n
+
+perfects :: Int -> [Int]
+perfects n = [x | x <- [1..n], perfect x]
+
+scalarProduct :: [Int] -> [Int] -> Int
+scalarProduct xs ys = sum [x * y | (x, y) <- zip xs ys]
